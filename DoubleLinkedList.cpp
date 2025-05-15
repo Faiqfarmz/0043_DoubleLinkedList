07970738f8cb354e64632895739b07ab0f34d351
Faiqfarmz <faiqfarmz@gmail.com>
2025-05-15 10:46:27 +0700

    step 1-5

--- DoubleLinkedList.cpp
+++ DoubleLinkedList.cpp
@@ -20,4 +20,28 @@ class DoubleLinkedList
         {
             START = NULL;
         }
+
+
+void addNode()
+    {
+        int nim;
+        string nm;
+        cout << "\nEnter the roll number of the student";
+        cin >> nim;
+        Node *newNode = new Node();
+        newNode -> noMhs = nim;
+        if (START == NULL || nim <= START->noMhs){
+            if (START != NULL && nim == START ->noMhs)
+            {
+                cout << "\nDuplicate number not allowed" << endl;
+                return;
+            }
+            //step: newNode.next = START
+            newNode-> next = START;
+
+            //step 5: start. prev = newNode (if start exist)
+            if (START != NULL)
+                START->prev = newNode;
+        }
+    }
 };
