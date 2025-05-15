b69cd8a8cfda98a13a1f1c9feb0efef6b8d00c4f
Faiqfarmz <faiqfarmz@gmail.com>
2025-05-15 11:05:59 +0700

    step 1

--- DoubleLinkedList.cpp
+++ DoubleLinkedList.cpp
@@ -86,7 +86,16 @@ void addNode()
         int rollNo;
         cin >> rollNo;
 
-        Node *current = START;
+                Node *current = START;
+        // step 1: Traverse the list to find the mode
+        while (current != NULL && current->noMhs != rollNo)
+        current = current->next;
+
+        if (current == NULL)
+        {
+        cout << "Record not found" << endl;
+        return;
+        }
 
 
     }
